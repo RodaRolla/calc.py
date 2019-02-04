9afa011d95a541b4e75df8f29bf036d38397aa17
Nikita Keler <47213208+RodaRolla@users.noreply.github.com>
2019-02-04 22:43:38 +0300

    Updated calc(not working)

--- calc2.py
+++ calc2.py
@@ -1,9 +1,30 @@
+#!python
+# coding: utf-8
 def printTop(stack):
 	try:
 		print (len(stack), stack[-1])
-	except Exception:
-		print ("Ой-ой! Стек пустой!")
+	except: Exception
+def multiplying(stack):
+	stack.append(stack.pop()*stack.pop())
 
+def addition(stack):
+	stack.append(stack.pop()+stack.pop())
+
+def subtraction(stack):
+	stack.append(-stack.pop()+stack.pop())
+
+def division(stack):
+	try:
+		stack.append((stack.pop()/stack.pop())**-1)
+	except ZeroDivisionError:
+		print('T_T')
+
+ops={
+	'+': addition,
+	'-': subtraction,
+	'*': multiplying,
+	'/': division
+}
 
 
 stack=[]
@@ -13,9 +34,12 @@ while True:
 			if token.isdigit():
 				stack.append(int(token))
 				printTop(stack)
-				x=(stack.pop()+stack.pop())
-				stack.append(x)
 			if token == 'stack':
 				print (stack)
+			continue
+			
+			if token in ops:
+				ops[token](stack)
+				printTop(stack)
 		except IndexError:
 			print ('4ota ne to')
