363bc2db76427fee4b0dc06b829fb1d78ae333c0
Nikita Keler <47213208+RodaRolla@users.noreply.github.com>
2019-02-11 23:20:14 +0300

    Add files via upload

---
+++ stackcalc.py
@@ -0,0 +1,44 @@
+#!python
+# coding: utf-8
+def printTop(stack):
+	try:
+		print (len(stack), stack[-1])
+	except: Exception
+def multiplying(stack):
+	stack.append(stack.pop()*stack.pop())
+
+def addition(stack):
+	stack.append(stack.pop()+stack.pop())
+
+def subtraction(stack):
+	stack.append(-stack.pop()+stack.pop())
+
+def division(stack):
+	try:
+		stack.append((stack.pop()/stack.pop())**-1)
+	except ZeroDivisionError:
+		print('T_T')
+
+ops={
+	'+': addition,
+	'-': subtraction,
+	'*': multiplying,
+	'/': division
+}
+
+
+stack=[]
+while True:
+	for token in input().split(' '):
+		try:
+			if token == 'stack':
+				print (stack)		
+			if token.isdigit():
+				stack.append(int(token))
+				printTop(stack)
+				continue
+			if token in ops:
+				ops[token](stack)
+				printTop(stack)
+		except IndexError:
+			print ('4ota ne to')
